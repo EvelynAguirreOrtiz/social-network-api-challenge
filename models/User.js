ecb24e2172dcacdf85e7bcabe69e3a45d6851a5a
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-05-26 18:26:29 -0500

    create user model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,38 @@
+const { Schema, model } = require("mongoose");
+// const dateFormat = require("../utils/dateFormat");
+
 // User
+const UserSchema = new Schema({
+	userName: {
+		type: String,
+		unique: true,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		validate: {
+			validator: function (v) {
+				return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid email!`,
+		},
+	},
+	// thoughts: [ThoughtSchema.id],
+	// Array of _id values referencing the Thought model
+	// friends: [UserSchema.id],
+	// Array of _id values referencing the User model (self-reference)
+});
 
-// username
-
-// String
-// Unique
-// Required
-// Trimmed
-// email
-
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
-
-// Array of _id values referencing the Thought model
-// friends
-
-// Array of _id values referencing the User model (self-reference)
+const User = model("User", UserSchema);
 
 // Schema Settings
-
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+// UserSchema.virtual("friendCount").get(function () {
+// 	return this.friends.reduce((total, friend) => total + friends.length + 1, 0);
+// });
+
+// export the User model
+module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,24 +1,33 @@
-// /api/users
+const router = require("express").Router();
+const {
+	getAllUser,
+	// getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+} = require("../../controllers/user-controller");
 
+// /api/users
 // GET all users
+// POST a new user
+router.route("/").get(getAllUser).post(createUser);
 
 // GET a single user by its _id and populated thought and friend data
+// router.route("/:id/:thoughtId").get(getUserById);
 
-// POST a new user:
+// PUT to update a user by its _id
+// DELETE to remove user by its _id
+router.route("/:id").put(updateUser).delete(deleteUser);
 
 // // example data
 // {
 //   "username": "lernantino",
 //   "email": "lernantino@gmail.com"
 // }
-// PUT to update a user by its _id
-
-// DELETE to remove user by its _id
 
 // BONUS: Remove a user's associated thoughts when deleted.
-
 // /api/users/:userId/friends/:friendId
-
 // POST to add a new friend to a user's friend list
-
 // DELETE to remove a friend from a user's friend list
+
+module.exports = router;
